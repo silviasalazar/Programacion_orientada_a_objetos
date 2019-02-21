ac27302a56a6b36de033c935ce97c0c65ecb714c
silviasalazar <47604124+silviasalazar@users.noreply.github.com>
2019-02-20 19:40:46 -0800

    Tarea1 de POO U1
    Tarea1 de POO, operaciones básicas con fracciones.

---
+++ tarea1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace tarea1
+{
+    class Frac
+    {
+        private int num;
+        private int den;
+
+        public Frac(int num, int den)
+        {
+            this.num = num;
+            this.den = den;
+        }
+        public Frac()
+        {
+            num = 1;
+            den = 1;
+        }
+        //SUMA
+        public static Frac operator +(Frac x, Frac y)
+        {
+            Frac result = new Frac(x.num * y.den + x.den * y.num, x.den * y.den);
+            return result;
+        }
+        //RESTA
+        public static Frac operator -(Frac x, Frac y)
+        {
+            Frac result = new Frac(x.num * y.den - x.den * y.num, x.den * y.den);
+            return result;
+        }
+        //MULTIPLICACIÓN
+        public static Frac operator *(Frac x, Frac y)
+        {
+            Frac result = new Frac(x.num * y.num , x.den * y.den);
+            return result;
+        }
+        //DIVISIÓN
+        public static Frac operator /(Frac x, Frac y)
+        {
+            Frac result = new Frac(x.num * y.den , x.den * y.num);
+            return result;
+        }
+        //IMPRIMIR
+        public void imprime()
+        {
+            Console.WriteLine("{0}/{1}", num, den);
+        }
+
+        public decimal a_decimal()
+        {
+            return num / den;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+             Frac a = new Frac(1,5);
+            Frac b = new Frac();
+            Frac c = a+b;
+            Frac d = a - b;
+            Frac e = a * b;
+            Frac f = a / b;
+
+            //IMPRIMIR DATOS
+            a.imprime();
+            b.imprime();
+            c.imprime();
+            d.imprime();
+            e.imprime();
+            f.imprime();
+
+            Console.ReadKey();
+        }
+    }
+}
